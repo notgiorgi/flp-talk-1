9fdc5c6834b84a536577e64c85bcc9a0932d5e00
Giorgi Bagdavadze <notgiorgi@gmail.com>
2025-05-12 11:07:52 +0400

    Update 07_ai_sdk.ts

--- src/07_ai_sdk.ts
+++ src/07_ai_sdk.ts
@@ -1,7 +1,7 @@
 // https://ai-sdk.dev/docs/ai-sdk-core
 import { z } from "zod";
 import { generateText, type CoreMessage, generateObject } from "ai";
-import { openai } from "@ai-sdk/openai";
+import { openai, createOpenAI } from "@ai-sdk/openai";
 import { anthropic } from "@ai-sdk/anthropic";
 import { SYSTEM_PROMPT } from "./misc";
 
@@ -13,17 +13,27 @@ let chatHistory: CoreMessage[] = [
   {
     role: "user",
     content:
-      "Who is a director of Conclave (2024)? also, please add it to my watchlist",
+      "Who is a director of Goodfellas (1990)? also, please add it to my watchlist",
   },
 ];
 
-const [response1, response2, response3] = await Promise.all([
+const ailab = createOpenAI({
+  name: "ailab.ge",
+  apiKey: process.env.AILAB_API_KEY,
+  baseURL: process.env.AILAB_BASE_URL,
+});
+
+const [response1, response2, response3, response4] = await Promise.all([
   generateText({
     model: openai("gpt-4o"),
     messages: chatHistory,
   }),
   generateText({
     model: anthropic("claude-3-5-sonnet-latest"),
+    messages: chatHistory,
+  }),
+  generateText({
+    model: ailab("kona-2.0"),
     messages: chatHistory,
   }),
   // Gen structured data
@@ -43,4 +53,5 @@ const [response1, response2, response3] = await Promise.all([
 
 console.log("\x1b[36mGPT-4o:\x1b[0m", response1.text);
 console.log("\x1b[35mClaude:\x1b[0m", response2.text);
-console.log("\n\n\x1b[33mGenerateObject\x1b[0m", response3.object);
+console.log("\n\n\x1b[33mAILab.ge\x1b[0m", response3.text);
+console.log("\n\n\x1b[33mGenerateObject\x1b[0m", response4.object);
