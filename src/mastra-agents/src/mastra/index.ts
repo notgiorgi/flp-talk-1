96d5dc4aab9d69ff7cafdeb7e9ac01beba38d2fc
Giorgi Bagdavadze <notgiorgi@gmail.com>
2025-05-28 21:07:42 +0400

    upgrade mastra

--- src/mastra-agents/src/mastra/index.ts
+++ src/mastra-agents/src/mastra/index.ts
@@ -1,13 +1,15 @@
 import { createLogger } from "@mastra/core/logger";
-import { LibSQLStore } from "@mastra/libsql";
+import { DefaultStorage } from "@mastra/libsql";
 import { Mastra } from "@mastra/core";
 import { movieAgent } from "./agents";
 
+const storage = new DefaultStorage({
+  url: ":memory:",
+});
+
 export const mastra = new Mastra({
   agents: { movieAgent },
-  storage: new LibSQLStore({
-    url: ":memory:",
-  }),
+  storage,
   logger: createLogger({
     name: "Quentin",
     level: "info",
